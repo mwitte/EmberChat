5407eaa6cd6c3b201d34be2cbad74e0da1039177
Matthias Witte <m.witte@techdivision.com>
2014-02-06 17:07:07 +0100

    fixed fonts path

--- Gruntfile.js
+++ Gruntfile.js
@@ -116,11 +116,11 @@ module.exports = function (grunt) {
                 generatedImagesDir: '.tmp/images/generated',
                 imagesDir: '<%= yeoman.app %>/images',
                 javascriptsDir: '<%= yeoman.app %>/scripts',
-                fontsDir: '<%= yeoman.app %>/styles/fonts',
+                fontsDir: '<%= yeoman.app %>/fonts',
                 importPath: 'app/bower_components',
                 httpImagesPath: '/images',
                 httpGeneratedImagesPath: '/images/generated',
-                httpFontsPath: '/styles/fonts',
+                httpFontsPath: '/fonts',
                 relativeAssets: false
             },
             dist: {},
@@ -253,7 +253,7 @@ module.exports = function (grunt) {
                         '*.{ico,txt}',
                         '.htaccess',
                         'images/{,*/}*.{webp,gif}',
-                        'styles/fonts/*'
+                        'fonts/*'
                     ]
                 }]
             }
